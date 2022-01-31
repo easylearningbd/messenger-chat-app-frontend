3140f016bd5cb55ee24120f075291aebd2444b25
easylearningbd <elebdsoft@gmail.com>
2022-02-01 02:29:25 +0600

    37 Messenger Design Part 5

---
+++ src/components/Message.jsx
@@ -0,0 +1,176 @@
+import React from 'react';
+
+const Message = () => {
+  return (
+       <div className='message-show'>
+
+            <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+              <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+
+              <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+              <div className='fd-message'>
+                   <div className='image-message-time'>
+                   <img src='/image/46668businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg' alt='' />
+                   <div className='message-time'>
+                        <div className='fd-text'>
+               <p className='message-text'>I am Fine </p>
+                        </div>
+                        <div className='time'>
+                                3 Jan 2022             
+                        </div>
+                   </div>
+                   </div>
+              </div>
+
+
+              <div className='fd-message'>
+                   <div className='image-message-time'>
+                   <img src='/image/46668businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg' alt='' />
+                   <div className='message-time'>
+                        <div className='fd-text'>
+               <p className='message-text'>I am Fine </p>
+                        </div>
+                        <div className='time'>
+                                3 Jan 2022             
+                        </div>
+                   </div>
+                   </div>
+              </div>
+
+
+
+              
+            <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+
+              
+            <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+              <div className='fd-message'>
+                   <div className='image-message-time'>
+                   <img src='/image/46668businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg' alt='' />
+                   <div className='message-time'>
+                        <div className='fd-text'>
+               <p className='message-text'>I am Fine </p>
+                        </div>
+                        <div className='time'>
+                                3 Jan 2022             
+                        </div>
+                   </div>
+                   </div>
+              </div>
+
+
+              
+            <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+
+              <div className='fd-message'>
+                   <div className='image-message-time'>
+                   <img src='/image/46668businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg' alt='' />
+                   <div className='message-time'>
+                        <div className='fd-text'>
+               <p className='message-text'>I am Fine </p>
+                        </div>
+                        <div className='time'>
+                                3 Jan 2022             
+                        </div>
+                   </div>
+                   </div>
+              </div>
+
+
+                   
+            <div className='my-message'>
+                 <div className='image-message'>
+                      <div className='my-text'>
+                           <p className='message-text'> How Are You? </p>
+                      </div>
+                 </div>
+                 <div className='time'>
+                       2 Jan 2022                      
+                 </div>
+              </div>
+
+
+
+              <div className='fd-message'>
+                   <div className='image-message-time'>
+                   <img src='/image/46668businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg' alt='' />
+                   <div className='message-time'>
+                        <div className='fd-text'>
+               <p className='message-text'>I am Fine </p>
+                        </div>
+                        <div className='time'>
+                                3 Jan 2022             
+                        </div>
+                   </div>
+                   </div>
+              </div>
+
+
+
+
+
+       </div>
+  )
+};
+
+export default Message;
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaPhoneAlt,FaVideo,FaRocketchat } from "react-icons/fa";
+import Message from './Message';
 
 const RightSide = () => {
   return (
@@ -34,8 +35,12 @@ const RightSide = () => {
 
     </div>
          </div>
+
+          <Message />
+
+
              </div>
-                      </div>  
+                 </div>  
 
                  <div className='col-4'>
                     User About Page 
