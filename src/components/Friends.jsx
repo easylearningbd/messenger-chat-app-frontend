8945e7d7443b3b9b70fa2e4a50b24eef911b93fd
easylearningbd <elebdsoft@gmail.com>
2022-02-18 04:16:09 +0600

    79 Show Last Message Part 1

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import moment from 'moment';
 
-const Friends = ({friend}) => {
+const Friends = (props) => {
+     const {fndInfo,msgInfo} = props.friend;
+     const myId = props.myId;
+
   return (
        <div className='friend'>
             <div className='friend-image'>
                  <div className='image'>
-                 <img src={`./image/${friend.fndInfo.image}`} alt='' />
+                 <img src={`./image/${fndInfo.image}`} alt='' />
                  </div>
             </div>
 
             <div className='friend-name-seen'>
                  <div className='friend-name'>
-                      <h4>{friend.fndInfo.userName}</h4>
+                      <h4>{fndInfo.userName}</h4>
+                      <div className='msg-time'>
+     {
+          msgInfo && msgInfo.senderId  === myId ? <span>You </span> : <span> {fndInfo.userName + ' '} </span>
+     }
+     {
+          msgInfo && msgInfo.message.text ? <span>{msgInfo.message.text.slice(0, 10)}</span> : msgInfo && msgInfo.message.image ? <span>Send A image </span> : <span>Connect You </span>
+     }
+     <span>{msgInfo ? moment(msgInfo.createdAt).startOf('mini').fromNow() : moment(fndInfo.createdAt).startOf('mini').fromNow()}</span>
+
+                      </div>
                  </div>
 
             </div>
